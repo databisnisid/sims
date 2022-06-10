b449a036b40b8a754baacd3fdfc528ff6690e047
gkiserpong <gkiserpongit@gmail.com>
2022-06-10 19:30:54 +0700

    Go green tea

--- connector/utils.py
+++ connector/utils.py
@@ -27,14 +27,14 @@ def get_parameter_status():
 
         if loc.parameter_2:
             print(result[loc.device.parameter_2.value])
-            loc.status_1 = result[loc.device.parameter_2.value]
+            loc.status_2 = result[loc.device.parameter_2.value]
 
         if loc.parameter_3:
             print(result[loc.device.parameter_3.value])
-            loc.status_1 = result[loc.device.parameter_3.value]
+            loc.status_3 = result[loc.device.parameter_3.value]
 
         if loc.parameter_4:
             print(result[loc.device.parameter_4.value])
-            loc.status_1 = result[loc.device.parameter_4.value]
+            loc.status_4 = result[loc.device.parameter_4.value]
 
         loc.save()
