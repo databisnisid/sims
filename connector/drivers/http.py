359858733698407d45e0322ec5c26007f78949d5
gkiserpong <gkiserpongit@gmail.com>
2022-06-14 11:52:39 +0700

    detect_black_image

--- connector/drivers/http.py
+++ connector/drivers/http.py
@@ -1,13 +1,30 @@
 import pycurl
 import certifi
-from io import BytesIO, StringIO
+import io
 import cv2
 import numpy as np
 
 
+def detect_black_image(buffer):
+    """ Detect Black Image. False if ALL BLACK. True if Images is NOT ALL BLACK """
+    buffer.seek(0)
+    file_bytes = np.asarray(bytearray(buffer.read()), dtype=np.uint8)
+    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    gray_version = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    if cv2.countNonZero(gray_version) != 0:
+        print("Image is fine")
+        is_image_ok = True
+    else:
+        print("Error")
+        is_image_ok = False
+
+    return is_image_ok
+
+
 def get(url):
     """ Check Channel and if picture snapshot is BLANK or not """
-    buffer = BytesIO()
+    buffer = io.BytesIO()
     c = pycurl.Curl()
     c.setopt(c.URL, url)
     c.setopt(c.WRITEDATA, buffer)
@@ -20,20 +37,10 @@ def get(url):
 
     c.close()
 
-    ''' Detect Image Black '''
+    # Detect Image Black
     is_image_ok = False
     if result:
-        buffer.seek(0)
-        file_bytes = np.asarray(bytearray(buffer.read()), dtype=np.uint8)
-        image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-        gray_version = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        if cv2.countNonZero(gray_version) != 0:
-            print("Image is fine")
-            is_image_ok = True
-        else:
-            print("Error")
-            is_image_ok = False
+        is_image_ok = detect_black_image(buffer)
 
     buffer.close()
 
